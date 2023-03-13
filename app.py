abfcca6983a2a9cd777a8225b88f0972d1a8f994
dpp0900 <_dpp@kakao.com>
2023-03-13 23:22:18 +0900

    가세혁

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import random
 import docker
 import datetime
+import time
 
 app = Flask(__name__)
 
@@ -16,17 +17,17 @@ def refresh_docker(deadtime):
     deadtime_delta = datetime.timedelta(hours=deadtime)
     deadtime_delta_seconds = deadtime_delta.total_seconds()
     containers = client.containers.list(all=True)
-    for container in containers:
-        start_time_str = container.attrs['Created']
-        start_time = datetime.datetime.strptime(start_time_str, '%Y-%m-%dT%H:%M:%S.%fZ')
-        livetime = (datetime.datetime.utcnow() - start_time).total_seconds()
-        if livetime > deadtime_delta_seconds:
-            container.stop()
-            container.remove()
+    if containers:
+        for container in containers:
+            start_time_str = container.attrs['Created']
+            start_time = datetime.datetime.strptime(start_time_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+            livetime = (datetime.datetime.utcnow() - start_time).total_seconds()
+            if livetime > deadtime_delta_seconds:
+                container.stop()
+                container.remove()
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    refresh_docker(4)
     if request.method == "GET":
         return render_template("index.html", hint=0)
     if request.method == "POST":
@@ -36,12 +37,10 @@ def index():
             return "<script>alert(\"Wrong!!\");history.back(-1);</script>"
 @app.route("/hint")
 def hint():
-    refresh_docker(4)
     return render_template("index.html", pf="pf")
 
 @app.route("/portf")
 def portf():
-    refresh_docker(4)
     with open("./templates/pf.pdf", "rb") as pdf:
         binary_pdf = pdf.read()
     response = make_response(binary_pdf)
@@ -52,10 +51,10 @@ def portf():
 
 @app.route("/prob")
 def prob():
-    refresh_docker(4)
     port = random.randint(49152,65535)
     os.popen(f"docker run --rm -p {port}:80 shock")
-    return render_template("index.html", link="http://localhost:" + str(port))
+    time.sleep(2)
+    return render_template("index.html", link="http://www.dpp0900.com:" + str(port))
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port="80")
